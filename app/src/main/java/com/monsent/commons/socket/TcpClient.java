4f0c861c073a4a2a07a72e4dcde482de2e33b9e0
hleluo <609060722@qq.com>
2018-05-22 16:01:01 +0800

    TCP

--- app/src/main/java/com/monsent/commons/socket/TcpClient.java
+++ app/src/main/java/com/monsent/commons/socket/TcpClient.java
@@ -8,14 +8,19 @@ import java.net.Socket;
 public class TcpClient {
 
     public interface Callback {
-        void onConnect(Socket socket, boolean success);
 
-        void onRead(Socket socket);
+        void onConnect();
+
+        void onReceive(byte[] bytes);
 
         void onDisconnect();
+
+        void onError(Exception e);
     }
 
+    private final static long MAX_NO_DATA_SECOND = 30 * 60L;    //最长未接收数据断开连接时长
     private Socket socket;
+    private long lastTime = 0L;     //最后接收数据时间
     private boolean readable = false;
     private Thread threadRead, threadConnect;
     private OutputStream os = null;
@@ -27,52 +32,97 @@ public class TcpClient {
     }
 
     /**
-     * 连接
+     * 连接服务端
      *
      * @param address 服务端地址
      * @param port    服务端端口号
-     * @return 是否连接成功
      */
-    public void connect(String address, int port) {
-        this.disconnect();
-        boolean result = false;
-        try {
-            socket = new Socket(address, port);
-            socket.setKeepAlive(true);
-            socket.setTcpNoDelay(true);
-            socket.setSoLinger(false, -1);
-            socket.setSoTimeout(10000);
-            result = true;
-            this.startRead();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (callback != null) {
-            callback.onConnect(socket, result);
-        }
-    }
-
-    private void startRead() {
-        if (threadRead == null) {
-            threadRead = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (readable) {
-                        try {
-                            is = socket.getInputStream();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+    public void connect(final String address, final int port) {
+        //停止连接
+        stopConnect();
+        //关闭socket
+        closeSocket();
+        threadConnect = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket(address, port);
+                    socket.setKeepAlive(true);
+                    socket.setTcpNoDelay(true);
+                    socket.setSoLinger(false, -1);
+                    socket.setSoTimeout(10000);
+                    lastTime = System.currentTimeMillis();
+                    //启动读线程
+                    startRead();
+                    if (callback != null) {
+                        callback.onConnect();
+                    }
+                } catch (IOException e) {
+                    handleError(e);
+                    if (callback != null) {
+                        callback.onDisconnect();
                     }
                 }
-            });
-        }
-        if (!readable) {
-            readable = true;
-            threadRead.start();
-        }
+            }
+        });
+        threadConnect.start();
     }
 
+    /**
+     * 停止连接线程
+     */
+    private void stopConnect() {
+        if (threadConnect != null && !threadConnect.isInterrupted()) {
+            threadConnect.interrupt();
+        }
+        threadConnect = null;
+    }
+
+    /**
+     * 启动读线程
+     */
+    private void startRead() {
+        stopRead();
+        threadRead = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                readable = true;
+                while (readable) {
+                    try {
+                        is = socket.getInputStream();
+                        int size = is.available();
+                        if (size > 0) {
+                            byte[] bytes = new byte[size];
+                            is.read(bytes);
+                            if (callback != null) {
+                                callback.onReceive(bytes);
+                            }
+                        } else {
+                            //{MAX_NO_DATA_SECOND}未收到任何数据，关闭连接
+                            if (System.currentTimeMillis() - lastTime > MAX_NO_DATA_SECOND * 1000) {
+                                disconnect();
+                                if (callback != null) {
+                                    callback.onDisconnect();
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        handleError(e);
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+        });
+        threadRead.start();
+    }
+
+    /**
+     * 停止读线程
+     */
     private void stopRead() {
         readable = false;
         if (threadRead != null && !threadRead.isInterrupted()) {
@@ -81,6 +131,28 @@ public class TcpClient {
         threadRead = null;
     }
 
+    /**
+     * 判断远程socket是否关闭
+     *
+     * @return 是否关闭
+     */
+    private boolean isSocketClosed() {
+        try {
+            socket.sendUrgentData(0xFF);
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
+    }
+
+    /**
+     * 写入数据
+     *
+     * @param bytes 字节数组
+     * @param off   起始位
+     * @param len   长度
+     * @return 是否成功
+     */
     public boolean write(byte[] bytes, int off, int len) {
         if (socket == null) {
             return false;
@@ -93,45 +165,88 @@ public class TcpClient {
             os.write(bytes, off, len);
             os.flush();
         } catch (IOException e) {
-            e.printStackTrace();
+            handleError(e);
             return false;
         }
         return true;
     }
 
+    /**
+     * 写入数据
+     *
+     * @param bytes 字节数组
+     * @return 是否成功
+     */
     public boolean write(byte[] bytes) {
         return write(bytes, 0, bytes.length);
     }
 
+    /**
+     * 写入数据
+     *
+     * @param message 字符串
+     * @return 是否成功
+     */
     public boolean write(String message) {
-        return write(message.getBytes());
+        return message != null && write(message.getBytes());
     }
 
-    public void disconnect() {
-        stopRead();
-        if (threadConnect != null && !threadConnect.isInterrupted()) {
-            threadConnect.interrupt();
+    /**
+     * 关闭socket
+     */
+    private void closeSocket() {
+        lastTime = 0L;
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                handleError(e);
+            }
+            socket = null;
         }
-        threadConnect = null;
+    }
+
+    /**
+     * 关闭数据流
+     */
+    private void closeStream() {
         if (is != null) {
             try {
                 is.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                handleError(e);
             }
         }
         if (os != null) {
             try {
                 os.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                handleError(e);
             }
         }
-        if (socket != null) {
+    }
+
+    /**
+     * 关闭连接
+     */
+    public void disconnect() {
+        stopConnect();
+        stopRead();
+        closeStream();
+        closeSocket();
+    }
+
+    /**
+     * 错误处理
+     *
+     * @param e 异常
+     */
+    private void handleError(Exception e) {
+        if (callback != null) {
             try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                callback.onError(e);
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
     }
--- app/src/main/java/com/monsent/commons/socket/TcpServer.java
+++ app/src/main/java/com/monsent/commons/socket/TcpServer.java
@@ -6,108 +6,167 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class TcpServer {
 
     public interface Callback {
 
-        void onAccept(String key, Socket socket);
+        void onAccept(Socket socket);
 
-        void onRead(String key, InputStream is);
+        void onReceive(Socket socket, byte[] bytes);
+
+        void onDisconnect(Socket socket);
+
+        void onError(Exception e);
 
     }
 
-    private static Map<String, Socket> mapSocket = new ConcurrentHashMap<>();
-    private static Map<String, Long> mapExpired = new ConcurrentHashMap<>();
-    private ServerSocket serverSocket;
-    private boolean accepted = false, readable = false;
-    private Thread threadAccept, threadRead;
+    private final static long MAX_NO_DATA_SECOND = 30 * 60L;    //最长未接收数据断开连接时长
+    private static Map<Socket, AtomicLong> mapSocket = new ConcurrentHashMap<Socket, AtomicLong>(); //socket、最后接收数据时间键值对
+    private ServerSocket serverSocket;  //服务端socket
+    private boolean accepted = false, readable = false;     //是否可以接收连接，是否继续读数据
+    private Thread threadAccept, threadRead;    //接收线程、读线程
     private OutputStream os = null;
     private InputStream is = null;
     private Callback callback;
-
-    public TcpServer(int port) {
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public TcpServer(int port, Callback callback) {
-        this(port);
-        this.callback = callback;
-    }
 
     public void setCallback(Callback callback) {
         this.callback = callback;
     }
 
-    public void startAccept() {
-        if (threadAccept == null) {
-            threadAccept = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (accepted) {
-                        try {
-                            Socket socket = serverSocket.accept();
-                            String key = UUID.randomUUID().toString();
-                            mapSocket.put(key, socket);
-                            if (callback != null) {
-                                callback.onAccept(key, socket);
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
+    /**
+     * 开启接收连接线程
+     *
+     * @param port 监听端口号
+     */
+    public void startAccept(final int port) {
+        stopAccept();
+        disconnectAll();
+        closeServerSocket();
+        threadAccept = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    serverSocket = new ServerSocket(port);
+                    accepted = true;
+                } catch (IOException e) {
+                    handleError(e);
+                }
+                while (accepted) {
+                    try {
+                        Socket socket = serverSocket.accept();
+                        mapSocket.put(socket, new AtomicLong(System.currentTimeMillis()));
+                        if (callback != null) {
+                            callback.onAccept(socket);
                         }
+                    } catch (IOException e) {
+                        handleError(e);
                     }
                 }
-            });
-        }
-        if (!accepted) {
-            accepted = true;
-            threadAccept.start();
-        }
+            }
+        });
+        threadAccept.start();
+        startRead();
     }
 
-    public void stopAccept() {
+    /**
+     * 停止接收连接线程
+     */
+    private void stopAccept() {
         accepted = false;
+        if (threadAccept != null && !threadAccept.isInterrupted()) {
+            threadAccept.interrupt();
+        }
         threadAccept = null;
     }
 
-    public void startRead() {
-        if (threadRead == null) {
-            threadRead = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (readable) {
-                        for (String key : mapSocket.keySet()) {
-                            try {
-                                is = mapSocket.get(key).getInputStream();
+    /**
+     * 开启读数据线程
+     */
+    private void startRead() {
+        stopRead();
+        threadRead = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                readable = true;
+                while (readable) {
+                    for (Socket socket : mapSocket.keySet()) {
+                        try {
+                            is = socket.getInputStream();
+                            int size = is.available();
+                            if (size > 0) {
+                                mapSocket.get(socket).set(System.currentTimeMillis());
+                                byte[] bytes = new byte[size];
+                                is.read(bytes);
                                 if (callback != null) {
-                                    callback.onRead(key, is);
+                                    callback.onReceive(socket, bytes);
                                 }
-                            } catch (IOException e) {
-                                e.printStackTrace();
+                            } else {
+                                //{MAX_NO_DATA_SECOND}未收到任何数据，关闭连接
+                                long lastTime = mapSocket.get(socket).get();
+                                if (System.currentTimeMillis() - lastTime > MAX_NO_DATA_SECOND * 1000) {
+                                    if (callback != null) {
+                                        callback.onDisconnect(socket);
+                                    }
+                                    disconnect(socket);
+                                }
                             }
+                        } catch (IOException e) {
+                            handleError(e);
                         }
                     }
+                    try {
+                        Thread.sleep(100);
+                    } catch (Exception e) {
+
+                    }
                 }
-            });
-        }
-        if (!readable) {
-            readable = true;
-            threadRead.start();
-        }
+            }
+        });
+        threadRead.start();
     }
 
-    public void stopRead() {
+    /**
+     * 停止读数据线程
+     */
+    private void stopRead() {
         readable = false;
+        if (threadRead != null && !threadRead.isInterrupted()) {
+            threadRead.interrupt();
+        }
         threadRead = null;
     }
 
+    /**
+     * 判断远程socket是否关闭
+     *
+     * @param socket socket
+     * @return 是否关闭
+     */
+    private boolean isSocketClosed(Socket socket) {
+        try {
+            socket.sendUrgentData(0xFF);
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
+    }
+
+    /**
+     * 向客户端写入数据
+     *
+     * @param socket 客户端
+     * @param bytes  字节数组
+     * @param off    起始位
+     * @param len    长度
+     * @return 是否写入成功
+     */
     public boolean write(Socket socket, byte[] bytes, int off, int len) {
+        if (socket == null) {
+            return false;
+        }
         if (bytes == null) {
             return true;
         }
@@ -116,93 +175,159 @@ public class TcpServer {
             os.write(bytes, off, len);
             os.flush();
         } catch (IOException e) {
-            e.printStackTrace();
+            handleError(e);
             return false;
         }
         return true;
     }
 
+    /**
+     * 向客户端写入数据
+     *
+     * @param socket 客户端
+     * @param bytes  字节数组
+     * @return 是否写入成功
+     */
     public boolean write(Socket socket, byte[] bytes) {
         return write(socket, bytes, 0, bytes.length);
     }
 
+    /**
+     * 向客户端写入数据
+     *
+     * @param socket  客户端
+     * @param message 字符串
+     * @return 是否写入成功
+     */
     public boolean write(Socket socket, String message) {
-        if (message == null) {
-            return true;
-        }
-        return write(socket, message.getBytes());
+        return message != null && write(socket, message.getBytes());
     }
 
-    public boolean write(String key, byte[] bytes, int off, int len) {
-        return write(mapSocket.get(key), bytes, 0, bytes.length);
-    }
-
-    public boolean write(String key, byte[] bytes) {
-        return write(mapSocket.get(key), bytes);
-    }
-
-    public boolean write(String id, String message) {
-        return write(mapSocket.get(id), message);
-    }
-
+    /**
+     * 向客户端广播写入数据
+     *
+     * @param bytes 字节数组
+     * @param off   起始位
+     * @param len   长度
+     */
     public void broadcast(byte[] bytes, int off, int len) {
-        for (Socket socket : mapSocket.values()) {
+        for (Socket socket : mapSocket.keySet()) {
             write(socket, bytes, off, len);
         }
     }
 
+    /**
+     * 向客户端广播写入数据
+     *
+     * @param bytes 字节数组
+     */
     public void broadcast(byte[] bytes) {
         broadcast(bytes, 0, bytes.length);
     }
 
+    /**
+     * 向客户端广播写入数据
+     *
+     * @param message 字符串
+     */
     public void broadcast(String message) {
         if (message != null) {
             broadcast(message.getBytes());
         }
     }
 
-    public void disconnect(Socket socket) {
-        if (socket != null) {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    /**
+     * 关闭客户端
+     *
+     * @param socket 客户端socket
+     */
+    private void closeSocket(Socket socket) {
+        if (socket == null) {
+            return;
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            handleError(e);
         }
     }
 
-    public void disconnect(String key) {
-        disconnect(mapSocket.get(key));
+    /**
+     * 断开客户端连接
+     *
+     * @param socket 客户端
+     */
+    public void disconnect(Socket socket) {
+        closeSocket(socket);
+        mapSocket.remove(socket);
     }
 
+    /**
+     * 断开所有客户端连接
+     */
     public void disconnectAll() {
-        for (Socket socket : mapSocket.values()) {
+        for (Socket socket : mapSocket.keySet()) {
             disconnect(socket);
         }
     }
 
-    public void close() {
-        stopAccept();
-        stopRead();
+    /**
+     * 关闭服务端连接
+     */
+    private void closeServerSocket() {
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                handleError(e);
+            }
+            serverSocket = null;
+        }
+    }
+
+    /**
+     * 关闭数据流
+     */
+    private void closeStream() {
         if (is != null) {
             try {
                 is.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                handleError(e);
             }
         }
         if (os != null) {
             try {
                 os.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                handleError(e);
             }
         }
+    }
+
+    /**
+     * 关闭连接
+     */
+    public void close() {
+        stopAccept();
+        stopRead();
+        closeStream();
         disconnectAll();
-        try {
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        closeServerSocket();
+    }
+
+    /**
+     * 错误处理
+     *
+     * @param e 异常
+     */
+    private void handleError(Exception e) {
+        if (callback != null) {
+            try {
+                callback.onError(e);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
